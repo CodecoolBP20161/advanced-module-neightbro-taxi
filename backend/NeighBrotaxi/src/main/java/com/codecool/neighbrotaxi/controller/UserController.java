832ece47dcb30caffdb83581ad65a4fabc1247b0
Ádám Horváth <horada1991@gmail.com>
2017-02-02 13:51:45 +0100

    Repository interfaces for User and Role entities

--- backend/NeighBrotaxi/src/main/java/com/codecool/neighbrotaxi/controller/UserController.java
+++ backend/NeighBrotaxi/src/main/java/com/codecool/neighbrotaxi/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.codecool.neighbrotaxi.controller;
 
 import com.codecool.neighbrotaxi.model.User;
+import com.codecool.neighbrotaxi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,7 +17,6 @@ public class UserController {
 
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
     public String registration(@ModelAttribute(value = "registrationForm") User user){
-        // TODO: implement the registration controller
         return null;
     }
 }
--- backend/NeighBrotaxi/src/main/java/com/codecool/neighbrotaxi/repository/RoleRepository.java
+++ backend/NeighBrotaxi/src/main/java/com/codecool/neighbrotaxi/repository/RoleRepository.java
@@ -1,10 +1,13 @@
 package com.codecool.neighbrotaxi.repository;
 
+import com.codecool.neighbrotaxi.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 /**
  * Created by cave on 2017.02.02..
  */
-@Repository
-public interface RoleRepository {
+@RepositoryRestResource(collectionResourceRel = "user-roles", path = "user-roles")
+public interface RoleRepository extends JpaRepository<Role, String>{
 }
