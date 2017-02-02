5fcae6256b09351798faf1636ab21f041c4bc520
Ádám Horváth <horada1991@gmail.com>
2017-02-02 14:33:54 +0100

    TAXI-28 UserService implementation
    + WebSecurityConfig

--- backend/NeighBrotaxi/src/main/java/com/codecool/neighbrotaxi/controller/UserController.java
+++ backend/NeighBrotaxi/src/main/java/com/codecool/neighbrotaxi/controller/UserController.java
@@ -1,15 +1,18 @@
 package com.codecool.neighbrotaxi.controller;
 
 import com.codecool.neighbrotaxi.model.User;
+import com.codecool.neighbrotaxi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.jws.soap.SOAPBinding;
+
 
 @RestController
 public class UserController {
-
 
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
     public String registration(@ModelAttribute(value = "registrationForm") User user){
