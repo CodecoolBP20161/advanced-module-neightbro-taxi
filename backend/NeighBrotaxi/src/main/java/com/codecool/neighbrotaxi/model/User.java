2b761d02798663ce8088a524556804b3bc48aadb
kalmeee <revkalman@gmail.com>
2017-02-02 12:16:07 +0100

    user entity

--- backend/NeighBrotaxi/src/main/java/com/codecool/neighbrotaxi/model/User.java
+++ backend/NeighBrotaxi/src/main/java/com/codecool/neighbrotaxi/model/User.java
@@ -1,6 +1,8 @@
 package com.codecool.neighbrotaxi.model;
 
 import javax.persistence.*;
+import java.util.Set;
+import java.util.UUID;
 
 /**
  * Created by cave on 2017.02.02..
@@ -8,4 +10,75 @@ import javax.persistence.*;
 @Entity
 @Table(name = "user")
 public class User {
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private String passwordConfirm;
+    private Set<Role> roles;
+
+
+    {
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public User() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public User(String name, String email, String password, String passwordConfirm, Set<Role> roles) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.passwordConfirm = passwordConfirm;
+        this.roles = roles;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
