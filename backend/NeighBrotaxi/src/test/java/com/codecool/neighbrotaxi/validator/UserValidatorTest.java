40ba69aa7d732efd59ce3e08e4665a53acdf1f91
kalmeee <revkalman@gmail.com>
2017-02-02 15:15:39 +0100

    TAXI-37 empty validator test

---
+++ backend/NeighBrotaxi/src/test/java/com/codecool/neighbrotaxi/validator/UserValidatorTest.java
@@ -0,0 +1,23 @@
+package com.codecool.neighbrotaxi.validator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kalman on 2017.02.02..
+ */
+public class UserValidatorTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void validate() throws Exception {
+
+    }
+
+}
