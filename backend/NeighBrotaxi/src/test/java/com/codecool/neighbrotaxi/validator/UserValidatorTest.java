4b096812faf5156e5dc2bc269ea5008affc4adcd
JacketWhite <feherkeee@gmail.com>
2017-02-03 11:17:37 +0100

    TAXI-37 first two validator test cases implemented

--- backend/NeighBrotaxi/src/test/java/com/codecool/neighbrotaxi/validator/UserValidatorTest.java
+++ backend/NeighBrotaxi/src/test/java/com/codecool/neighbrotaxi/validator/UserValidatorTest.java
@@ -1,23 +1,63 @@
 package com.codecool.neighbrotaxi.validator;
 
+import com.codecool.neighbrotaxi.AbstractTest;
+import com.codecool.neighbrotaxi.model.User;
+import com.codecool.neighbrotaxi.service.UserService;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.Errors;
 
-import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by kalman on 2017.02.02..
  */
-public class UserValidatorTest {
+@MockBean(UserService.class)
+@MockBean(Errors.class)
+@Transactional
+public class UserValidatorTest extends AbstractTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private Errors errors;
+
+    @Autowired
+    private UserValidator userValidator;
+
+    private User user;
 
     @Before
     public void setUp() throws Exception {
-
+        user = new User();
+        user.setEmail("email");
+        user.setPassword("password");
+        user.setPasswordConfirm("password");
     }
 
     @Test
-    public void validate() throws Exception {
+    public void validate_AllFieldValid_NoErrorsInBinding() throws Exception {
 
+        userValidator.validate(user, errors);
+
+        verify(errors, never()).rejectValue(anyString(), anyString());
+        verify(errors, never()).rejectValue(anyString(), anyString(), anyString());
+    }
+
+    @Test
+    public void validate_EmailFieldNotValid_EmailAlreadyInUse() {
+        when(userService.findByEmail(anyString())).thenReturn(new User());
+
+        userValidator.validate(user, errors);
+
+        verify(errors).rejectValue("email", "Duplicate.user.email", "Email already in database");
     }
 
 }
