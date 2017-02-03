ec0121d2766a76070d2fd8c5250da770b4c58f0f
Ádám Horváth <horada1991@gmail.com>
2017-02-03 11:16:25 +0100

    TAXI-29 registration controller tests

---
+++ backend/NeighBrotaxi/src/test/java/com/codecool/neighbrotaxi/controller/UserControllerTest.java
@@ -0,0 +1,76 @@
+package com.codecool.neighbrotaxi.controller;
+
+import com.codecool.neighbrotaxi.AbstractTest;
+import com.codecool.neighbrotaxi.model.User;
+import com.codecool.neighbrotaxi.service.UserService;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by cave on 2017.02.03..
+ */
+@Transactional
+@MockBean(UserService.class)
+@MockBean(BindingResult.class)
+public class UserControllerTest extends AbstractTest {
+    @Autowired
+    private UserService userService;
+
+    private User user;
+
+    @Autowired
+    private UserController userController;
+
+    @Autowired
+    private BindingResult bindingResult;
+
+    @Before
+    public void setUp() throws Exception {
+        user = new User();
+        user.setName("name");
+        user.setPassword("pw");
+        user.setEmail("email");
+
+    }
+
+    @Test
+    public void registration_HasErrorsInBindingResult_ReturnListOfErrors() throws Exception {
+        ArrayList<ObjectError> listOfErrors = new ArrayList<>();
+        when(bindingResult.hasErrors()).thenReturn(true);
+        when(bindingResult.getAllErrors()).thenReturn(listOfErrors);
+
+        Object object = userController.registration(user, bindingResult);
+
+        assertEquals(listOfErrors, object);
+    }
+
+    @Test
+    public void registration_HasNoErrorsInBindingResult_CallSaveUser() throws Exception {
+        when(bindingResult.hasErrors()).thenReturn(false);
+
+        User returnedUser = (User) userController.registration(user, bindingResult);
+
+        verify(userService).save(user);
+    }
+
+    @Test
+    public void registration_HasNoErrorsInBindingResult_ReturnSavedUser() throws Exception {
+        when(bindingResult.hasErrors()).thenReturn(false);
+        when(userService.findOne(user.getId())).thenReturn(user);
+
+        User returnedUser = (User) userController.registration(user, bindingResult);
+
+        assertEquals(user, returnedUser);
+    }
+}
