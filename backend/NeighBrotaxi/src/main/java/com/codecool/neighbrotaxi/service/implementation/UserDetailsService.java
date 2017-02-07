8ff57b8b3b4856d7e3008674b6683b6b46d86496
Ádám Horváth <horada1991@gmail.com>
2017-02-07 14:22:39 +0100

    UserServiceDetailImpl implementation

--- backend/NeighBrotaxi/src/main/java/com/codecool/neighbrotaxi/service/implementation/UserDetailsService.java
+++ backend/NeighBrotaxi/src/main/java/com/codecool/neighbrotaxi/service/implementation/UserDetailsService.java
@@ -1,16 +1,33 @@
 package com.codecool.neighbrotaxi.service.implementation;
 
+import com.codecool.neighbrotaxi.model.Role;
+import com.codecool.neighbrotaxi.model.User;
 import com.codecool.neighbrotaxi.repository.UserRepository;
+import com.codecool.neighbrotaxi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Service
 public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return null;
+        User user = userRepository.findByEmail(email);
+
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        for (Role role: user.getRoles()){
+            grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), grantedAuthorities);
     }
 }
