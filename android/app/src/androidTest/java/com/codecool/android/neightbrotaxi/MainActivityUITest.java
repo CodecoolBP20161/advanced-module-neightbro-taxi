c2a3aa7a66e05b38a03a443d2edd22e5f0de6dcd
matolaypal <matolaypal@gmail.com>
2017-02-10 01:23:52 +0100

    TAXI-69 all empty input fields errors TEST OK

--- android/app/src/androidTest/java/com/codecool/android/neightbrotaxi/MainActivityUITest.java
+++ android/app/src/androidTest/java/com/codecool/android/neightbrotaxi/MainActivityUITest.java
@@ -4,40 +4,84 @@ import android.support.design.widget.TextInputLayout;
 import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-
 import com.codecool.android.neightbrotaxi.view.MainActivity;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static org.hamcrest.Matchers.is;
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
+import static android.support.test.espresso.action.ViewActions.clearText;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.core.deps.guava.base.Preconditions.checkNotNull;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.hamcrest.Matchers.is;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityUITest {
-    public static final String TAG = MainActivityUITest.class.getSimpleName()+"<>";
 
-    public static Matcher<View> withErrorInInputLayout(final Matcher<String> stringMatcher) {
+    @Rule
+    public ActivityTestRule<MainActivity> mRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void validateEmptyName() {
+        onView(withId(R.id.input_name)).perform(clearText());
+        onView(withId(R.id.btn_signup)).perform(click());
+        onView(withId(R.id.input_layout_name))
+                .check(matches(simulateEmptyInputError(mRule.getActivity().getString(R.string.err_name))));
+    }
+
+    @Test
+    public void validateEmptyEmail() {
+        onView(withId(R.id.input_name)).perform(typeText("Test User"), closeSoftKeyboard());
+        onView(withId(R.id.input_email)).perform(clearText());
+        onView(withId(R.id.btn_signup)).perform(click());
+        onView(withId(R.id.input_layout_email))
+                .check(matches(simulateEmptyInputError(mRule.getActivity().getString(R.string.err_email))));
+    }
+
+    @Test
+    public void validateEmptyPassword() {
+        onView(withId(R.id.input_name)).perform(typeText("Test User"), closeSoftKeyboard());
+        onView(withId(R.id.input_email)).perform(typeText("test@email.com"), closeSoftKeyboard());
+        onView(withId(R.id.input_password1)).perform(clearText());
+        onView(withId(R.id.btn_signup)).perform(click());
+        onView(withId(R.id.input_layout_password1))
+                .check(matches(simulateEmptyInputError(mRule.getActivity().getString(R.string.err_pw_empty))));
+    }
+
+    @Test
+    public void validateEmptyPasswordConfirm() {
+        onView(withId(R.id.input_name)).perform(typeText("Test User"), closeSoftKeyboard());
+        onView(withId(R.id.input_email)).perform(typeText("test@email.com"), closeSoftKeyboard());
+        onView(withId(R.id.input_password1)).perform(typeText("password"), closeSoftKeyboard());
+        onView(withId(R.id.input_password2)).perform(clearText());
+        onView(withId(R.id.btn_signup)).perform(click());
+        onView(withId(R.id.input_layout_password2))
+                .check(matches(simulateEmptyInputError(mRule.getActivity().getString(R.string.err_pw_same))));
+    }
+
+    public static Matcher<View> simulateEmptyInputError(final String string) {
+        return simulateEmptyInputError(is(string));
+    }
+
+    public static Matcher<View> simulateEmptyInputError(final Matcher<String> stringMatcher) {
         checkNotNull(stringMatcher);
 
         return new BoundedMatcher<View, TextInputLayout>(TextInputLayout.class) {
-            String actualError = "";
+
+            String errMsg = null;
 
             @Override
             public void describeTo(Description description) {
-                description.appendText("description: "+description);
                 stringMatcher.describeTo(description);
             }
 
@@ -45,30 +89,11 @@ public class MainActivityUITest {
             public boolean matchesSafely(TextInputLayout textInputLayout) {
                 CharSequence error = textInputLayout.getError();
                 if (error != null) {
-                    actualError = error.toString();
-                    return stringMatcher.matches(actualError);
+                    errMsg = error.toString();
+                    return stringMatcher.matches(errMsg);
                 }
                 return false;
             }
         };
-    }
-
-    public static Matcher<View> withErrorInInputLayout(final String string) {
-        return withErrorInInputLayout(is(string));
-    }
-
-    @Rule
-    public ActivityTestRule<MainActivity> mRule =
-            new ActivityTestRule<>(MainActivity.class);
-
-    @Test
-    public void validateName() {
-        String stringInput = "";
-//        onView(withId(R.id.input_name)).perform(typeText(stringInput));
-//
-//        onView(withId(R.id.input_name)).perform(pressImeActionButton());
-
-//        onView(withId(R.id.input_layout_name))
-//                .check(matches(viewMatcher(String.valueOf(mRule.getActivity().getText(R.string.err_name)))));
     }
 }
