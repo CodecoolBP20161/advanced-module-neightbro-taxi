311c0d14eeaea70e3cd295ce7ef51deb29c61633
matolaypal <matolaypal@gmail.com>
2017-02-13 22:37:06 +0100

    TAXI-63 AlertUserError documentation DONE

--- android/app/src/main/java/com/codecool/android/neightbrotaxi/model/AlertUserError.java
+++ android/app/src/main/java/com/codecool/android/neightbrotaxi/model/AlertUserError.java
@@ -2,17 +2,34 @@ package com.codecool.android.neightbrotaxi.model;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.util.Log;
 
+import java.util.Arrays;
+
+/**
+ * Responsible for notify the user, when something went wrong in the app.
+ */
 public class AlertUserError {
+    private final static String TAG = AlertUserError.class.getSimpleName()+"<>";
 
+    /**
+     * Build a Dialog and show it on the actual screen.
+     * @param activity from the current context.
+     * @param strings list with the Dialog details.
+     */
     private static void buildDialog (Activity activity, String ... strings) {
         AlertDialog.Builder builder = new AlertDialog.Builder(activity)
                 .setTitle(strings[0])
                 .setMessage(strings[1])
                 .setPositiveButton(strings[2], null);
         builder.create().show();
+        Log.i(TAG, "Dialog created: "+ Arrays.toString(strings));
     }
 
+    /**
+     * If user send a request, but no internet connection.
+     * @param activity from the current context.
+     */
     public static void connection (Activity activity)  {
         buildDialog(activity,
                 "Connection Error!",
@@ -20,6 +37,10 @@ public class AlertUserError {
                 "Okay");
     }
 
+    /**
+     * If user send a request, but server doesn't response.
+     * @param activity from the current context.
+     */
     public static void server (Activity activity)  {
         buildDialog(activity,
                 "Server Error!",
