e72913ae2102ebed0fa583f6f4105aa87c859036
matolaypal <matolaypal@gmail.com>
2017-02-13 19:47:24 +0100

    setup DONE, one ignored

---
+++ android/app/src/test/java/com/codecool/android/neightbrotaxi/view/MainActivityTest.java
@@ -0,0 +1,48 @@
+package com.codecool.android.neightbrotaxi.view;
+
+import android.app.Activity;
+import android.support.design.widget.TextInputLayout;
+import android.widget.EditText;
+
+import com.codecool.android.neightbrotaxi.BuildConfig;
+import com.codecool.android.neightbrotaxi.R;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 25)
+public class MainActivityTest {
+
+    private Activity mActivity;
+
+    @Before
+    public void setUp() throws Exception {
+        mActivity = Robolectric.setupActivity(MainActivity.class);
+        mActivity.setContentView(R.layout.activity_main);
+    }
+
+    @Ignore
+    @Test
+    public void validateName() throws Exception {
+        String errorMsg = mActivity.getString(R.string.err_name);
+
+        EditText inputName = (EditText) mActivity.findViewById(R.id.input_name);
+
+        TextInputLayout inputLayoutName =
+                (TextInputLayout) mActivity.findViewById(R.id.input_layout_name);
+
+        inputName.setText(null);
+
+//        inputName.addTextChangedListener(new MainActivity.MyTextWatcher(inputName));
+
+        assertEquals(errorMsg, inputLayoutName.getError());
+    }
+}
