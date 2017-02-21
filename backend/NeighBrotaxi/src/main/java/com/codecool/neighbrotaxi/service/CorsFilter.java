17803c580284dc7dca872bfebcc13d4a9d831d19
Ádám Horváth <horada1991@gmail.com>
2017-02-21 10:36:58 +0100

    Headers removed in backend

--- backend/NeighBrotaxi/src/main/java/com/codecool/neighbrotaxi/service/CorsFilter.java
+++ backend/NeighBrotaxi/src/main/java/com/codecool/neighbrotaxi/service/CorsFilter.java
@@ -1,27 +1,27 @@
-package com.codecool.neighbrotaxi.service;
-
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@Component
-public class CorsFilter extends OncePerRequestFilter {
-    @Override
-    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
-        httpServletResponse.addHeader("Access-Control-Allow-Origin", "*");
-        if (httpServletRequest.getHeader("Access-Control-Request-Method") != null
-                && "OPTIONS".equals(httpServletRequest.getMethod())) {
-            // CORS "pre-flight" httpServletRequest
-            httpServletResponse.addHeader("Access-Control-Allow-Methods",
-                    "GET, POST, PUT, DELETE");
-            httpServletResponse.addHeader("Access-Control-Allow-Headers",
-                    "X-Requested-With,Origin,Content-Type, Accept");
-        }
-        filterChain.doFilter(httpServletRequest, httpServletResponse);
-    }
-}
+//package com.codecool.neighbrotaxi.service;
+//
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.filter.OncePerRequestFilter;
+//
+//import javax.servlet.FilterChain;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+//@Component
+//public class CorsFilter extends OncePerRequestFilter {
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+//        httpServletResponse.addHeader("Access-Control-Allow-Origin", "http://localhost:63342");
+//        if (httpServletRequest.getHeader("Access-Control-Request-Method") != null
+//                && "OPTIONS".equals(httpServletRequest.getMethod())) {
+//            // CORS "pre-flight" httpServletRequest
+//            httpServletResponse.addHeader("Access-Control-Allow-Methods",
+//                    "GET, POST, PUT, DELETE");
+//            httpServletResponse.addHeader("Access-Control-Allow-Credentials", "true");
+//            httpServletResponse.addHeader("Access-Control-Allow-Headers", "X-Requested-With,Origin,Content-Type, Accept");
+//        }
+//        filterChain.doFilter(httpServletRequest, httpServletResponse);
+//    }
+//}
