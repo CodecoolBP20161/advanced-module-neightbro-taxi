852c78f6b04a75a7acfa6be6bb743a2a73b7a1d4
beru26 <berkes.edina@gmail.com>
2017-02-21 11:47:22 +0100

    TAXI-107 add httpProvider to app

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -1,3 +1,5 @@
 'use strict';
 
-angular.module('neighbroTaxi', ['ngRoute']);
+angular.module('neighbroTaxi', ['ngRoute']).config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.withCredentials = true;
+}]);
