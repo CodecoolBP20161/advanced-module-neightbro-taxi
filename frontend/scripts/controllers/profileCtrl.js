3851b47fbcef62fc51dfc7af6d79d34172ac4a05
annakertesz <kerteszannanak@gmail.com>
2017-03-01 12:10:18 +0100

    TAXI-113 profilectrl and html ready, need css

--- frontend/scripts/controllers/profileCtrl.js
+++ frontend/scripts/controllers/profileCtrl.js
@@ -1,8 +1,17 @@
-'use strict'
+'use strict';
 
 angular.module('neighbroTaxi')
 
 .controller('profileCtrl', function ($scope, $http) {
+
+    $scope.onEdit = false;
+
+    // $scope.inUser.phone="454-1234-345";
+    // $scope.inUser.car="Ford fiesta, 5 hely";
+
+    $scope.changeEditing = function() {
+        $scope.onEdit = !$scope.onEdit;
+    };
 
     $http.get('http://localhost:9000/logged-in-user')
         .success(function (response) {
@@ -13,4 +22,17 @@ angular.module('neighbroTaxi')
             console.log(response);
     });
 
-})
+    $scope.editUser = function () {
+
+
+        console.log($scope.inUser);
+        $http.put('http://localhost:9000/update-user', $scope.inUser).
+        success(function (data) {
+            console.log("Success");
+        }).error(function (data) {
+            console.log(":(");
+        });
+        $scope.changeEditing();
+    }
+
+});
