86b533882e22b38734aa3026c52ed1e5a5167354
matolaypal <matolaypal@gmail.com>
2017-03-11 23:15:25 +0100

    TAXI-96, TAXI-97, TAXI-109 logout OK, profile preparation

--- android/app/src/main/java/com/codecool/android/neightbrotaxi/view/MainActivity.java
+++ android/app/src/main/java/com/codecool/android/neightbrotaxi/view/MainActivity.java
@@ -1,22 +1,22 @@
 package com.codecool.android.neightbrotaxi.view;
 
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.PopupMenu;
 import android.util.Log;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.codecool.android.neightbrotaxi.R;
+import com.codecool.android.neightbrotaxi.controller.StorageController;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName() + "<>";
 
-    private DrawerLayout drawerLayout;
-    private ImageView imageView;
+    private StorageController storageController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,66 +27,41 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitleTextColor(getColor(R.color.colorPrimary));
         setSupportActionBar(toolbar);*/
-
-//        imageView = (ImageView) findViewById(R.id.icon_menu);
-//        imageView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                LayoutInflater layoutInflater = (LayoutInflater) getBaseContext()
-//                        .getSystemService(LAYOUT_INFLATER_SERVICE);
-//
-//                View popupView = layoutInflater.inflate(R.layout.popup, null);
-//
-//                final PopupWindow popupWindow = new PopupWindow(popupView,
-//                        RelativeLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-//
-////                popupWindow.showAsDropDown(mButton, 0, v.getHeight());
-//
-//                ListView list = (ListView) findViewById(R.id.list);
-//
-//                list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//
-//                    @Override
-//                    public void onItemClick(AdapterView<?> arg0, View arg1,
-//                                            int arg2, long arg3) {
-//                        // SOME YOUR CODE
-////                        Toast.makeText(getApplicationContext(), "button pressed", Toast.LENGTH_SHORT);
-//
-//                        ArrayList<String> myStringArray1 = new ArrayList<String>();
-//                        myStringArray1.add("Andrea");
-//                        adapter = new RecyclerView.Adapter(getActivity(), R.layout.row, myStringArray1);
-//                        myListView.setAdapter(adapter);
-//                        adapter.notifyDataSetChanged();
-//                        popupWindow.dismiss();
-//                    }
-//                });
-//            }
-//        });
-
-
     }
 
-
     public void showPopup(View v) {
+        storageController = new StorageController(getApplicationContext());
         PopupMenu popup = new PopupMenu(this, v);
         MenuInflater inflater = popup.getMenuInflater();
         inflater.inflate(R.menu.main, popup.getMenu());
-        //        popup.setOnMenuItemClickListener(this);
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.profile:
+                        getProfile();
+                        break;
+                    case R.id.logout:
+                        logout();
+                        break;
+                }
+                return false;
+            }
+        });
         popup.show();
     }
 
-//    @Override
-//    public boolean onMenuItemClick(MenuItem item) {
-//        switch (item.getItemId()) {
-//            case R.id.action_settings:
-//                Toast.makeText(getApplicationContext(), "clicked", Toast.LENGTH_SHORT).show();
-//            default:
-//            return false;
-//        }
-//    }
+    private void logout() {
+        Toast.makeText(getApplicationContext(), "logout clicked", Toast.LENGTH_SHORT).show();
+        storageController.removeUser();
+        finish();
+    }
 
-    public void logout(View v){
+    private void getProfile() {
+        storageController = new StorageController(getApplicationContext());
 
+        Toast.makeText(getApplicationContext(), storageController.getStoredUser().toString(),
+                Toast.LENGTH_SHORT).show();
     }
 
     /**
@@ -125,14 +100,4 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         Log.i(TAG, "ACTIVITY DESTROYED!");
     }
-
-    /**
-     * This method will be invoked when a menu item is clicked if the item
-     * itself did not already handle the event.
-     *
-     * @param item the menu item that was clicked
-     * @return {@code true} if the event was handled, {@code false}
-     * otherwise
-     */
-
 }
