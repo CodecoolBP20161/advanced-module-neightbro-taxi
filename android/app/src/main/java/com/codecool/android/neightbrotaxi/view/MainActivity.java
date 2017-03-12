893e22f4ebbb9b2b0ebb95ed1ae182c1b10b0682
matolaypal <matolaypal@gmail.com>
2017-03-12 02:13:13 +0100

    TAXI-97 menu behaviors well

--- android/app/src/main/java/com/codecool/android/neightbrotaxi/view/MainActivity.java
+++ android/app/src/main/java/com/codecool/android/neightbrotaxi/view/MainActivity.java
@@ -1,19 +1,68 @@
 package com.codecool.android.neightbrotaxi.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
 import android.util.Log;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.codecool.android.neightbrotaxi.R;
+import com.codecool.android.neightbrotaxi.controller.StorageController;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName() + "<>";
+    private StorageController storageController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        storageController = new StorageController(getApplicationContext());
+
+        Log.i(TAG, "ACTIVITY CREATED!");
+/*
+        // Set to the right color for the toolbar.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(getColor(R.color.colorPrimary));
+        setSupportActionBar(toolbar);*/
+    }
+
+    public void showPopup(View v) {
+        storageController = new StorageController(getApplicationContext());
+        PopupMenu popup = new PopupMenu(this, v);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.main, popup.getMenu());
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.profile:
+                        getProfile();
+                        break;
+                    case R.id.logout:
+                        logout();
+                        break;
+                }
+                return false;
+            }
+        });
+        popup.show();
+    }
+
+    private void logout() {
+        Toast.makeText(getApplicationContext(), "logout clicked", Toast.LENGTH_SHORT).show();
+        storageController.removeUser();
+        finish();
+    }
+
+    private void getProfile() {
+        Toast.makeText(getApplicationContext(), storageController.getStoredUser().toString(),
+                Toast.LENGTH_SHORT).show();
     }
 
     /**
@@ -26,6 +75,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         Log.i(TAG, "ACTIVITY STARTED!");
+
+        try {
+            Log.i(TAG, "IN SESSION: " + storageController.getStoredUser().toString());
+            storageController.getStoredUser();
+        }
+        catch (NullPointerException e ) {
+            Log.e(TAG, e.getMessage());
+            Intent intent = new Intent(MainActivity.this, AuthenticatorActivity.class);
+            startActivity(intent);
+        }
     }
 
     @Override
