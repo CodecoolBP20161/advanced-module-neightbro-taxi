15588c79f122852c0d3fd5af55696b690b1c8d79
JacketWhite <feherkeee@gmail.com>
2017-03-13 13:19:47 +0100

    TAXI-75 AdminService methods db transactions for add role to users

--- backend/NeighBrotaxi/src/main/java/com/codecool/neighbrotaxi/service/AdminService.java
+++ backend/NeighBrotaxi/src/main/java/com/codecool/neighbrotaxi/service/AdminService.java
@@ -5,6 +5,7 @@ import com.codecool.neighbrotaxi.model.Role;
 import com.codecool.neighbrotaxi.model.User;
 
 import java.util.List;
+import java.util.Set;
 
 public interface AdminService {
     List<User> getAllUser();
@@ -12,4 +13,6 @@ public interface AdminService {
     void addRole(Role role);
     void deleteUser(Integer userID);
     boolean deleteRole(Integer roleID);
+    User getUser(Integer userID);
+    void addRoleToUser(Set<Role> roles, Integer userID);
 }
