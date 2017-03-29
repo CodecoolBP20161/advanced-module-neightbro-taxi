70fe194ebd9489e7d5c53e6c7121b06026c9de24
annakertesz <kerteszannanak@gmail.com>
2017-03-29 15:27:47 +0200

    i leave this invalid branch

--- frontend/scripts/controllers/menuCtrl.js
+++ frontend/scripts/controllers/menuCtrl.js
@@ -4,21 +4,6 @@ angular.module('neighbroTaxi')
 
 .controller('menuCtrl', function ($scope) {
 
-    $scope.showDropDown = true;
-
-    $scope.showLogin = true;
-    $scope.showReg = false;
-
-    $scope.login = function () {
-        $scope.showLogin = true;
-        $scope.showReg = false;
-    };
-
-    $scope.reg = function () {
-        $scope.style = false;
-        $scope.showLogin = false;
-        $scope.showReg = true;
-        $scope.style = true;
-    };
+    $scope.showMenu = true;
 
 });
